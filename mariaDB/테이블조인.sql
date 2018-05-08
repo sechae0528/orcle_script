3aefbf0f07fdefe73e776326477825706edd8118
sungeun0528 <30641853+sungeun0528@users.noreply.github.com>
2018-05-08 14:45:30 +0900

    18.05.08

--- mariaDB/테이블조인.sql
+++ mariaDB/테이블조인.sql
@@ -12,7 +12,7 @@ select * from flight a, aircraft b
 		and a.flightName = 'OZ800';
 	 
 	 
--- ------------------------------예제 -------------------------------	 
+-- ------------------------------예제 ----------------------------------------------------------	 
 -- 1. 관리자인 회원의 id, name만 출력하세요 (0이라는 코드를 쓰면 안됨)
 select * from member a, auth b
 	where a.authcode = b.authcode
@@ -23,8 +23,44 @@ select b.title, a.name  from member a, board b
 	where a.id = b.id;
 
 
+-- 테이블 3개 조인할 때,
 -- 3. 권한이 '회원'인 회원이름, 회원id, 글제목, 글내용, 권한만 출력하세요(2라는 코드를 쓰면 안됨)
 select a.name, a.id, c.title, c.content, b.authority from member a, auth b, board c
 		where a.authcode = b.authcode
 	 		and a.id = c.id
 	 		and b.authority = '회원';
+	 		
+	 		
+-- ---------------------------------------------------------------------------------------------	 		
+-- left join 문법
+-- ------------------------------예제 -----------------------------------------------------------	 
+select * from flight a
+left join aircraft b
+	on a.aircraftCode = b.aircraftCode;
+	
+
+-- 1. 회원이름, 권한 출력 (회원전체)
+select a.name, b.authority from member a
+left join auth b
+	on a.authcode = b.authcode;
+	
+-- 2. 제목, 작성자 이름 출력 (글 전체)
+select a.title, b.name from board a
+left join member b
+	on a.id = b.id;
+	
+-- ----------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- 데이터조회.sql
+++ 데이터조회.sql
@@ -202,3 +202,144 @@ FROM KOPO_PROMOTION;
 ------LIKE : 특정 패턴을 가지고 있는 조건을 검색 
 -------(예 > LIKE '%MST%' : MST란 단어가 있는 데이터는 모두 검색) 
 ------IS NULL/ IS NOT NULL : Null 값을 검색 / Null 이 아닌 값을 검색
+
+
+--------------------------------------------------------------------------
+-- 사용자 조건 입력받기
+--SELECT
+--   {컬럼 리스트}
+--FROM KOPO_PRODUCT_VOLUME
+--WHERE 컬럼명 = &colon
+--------------------------------------------------------------------------
+-- PRODUCT에 지정값을 넣어서 조회하기 --
+SELECT
+   REGIONID
+   , PRODUCTGROUP
+   , YEARWEEK
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = &PRODUCT
+OR YEARWEEK = '201544';
+
+--- 예제 : KOPO_PROMOTION 테이블에서 STARTWEEK ENDWEEK 2개의
+--- -------사용자 변수를 입력 받아 201501, 201503 사이 데이터를 조회하세요.
+
+SELECT * from KOPO_PROMOTION;
+
+SELECT REGIONID
+   , ITEM
+   , TARGETWEEK
+   , PLANWEEK
+   , MAP_PRICE
+   , IR
+   , PMAP
+   , PRODUCT
+FROM KOPO_PROMOTION
+WHERE 1=1
+AND TARGETWEEK BETWEEN &STARTWEEK AND &ENDWEEK;
+
+---------------------------------------------------------------------------
+-- 정렬하기
+-- 기본 값은 오름차순
+-- ASC[오름차순] / DESC [내림차순]
+---------------------------------------------------------------------------
+--SELECT
+--   {컬럼 리스트}
+--FROM KOPO_PRODUCT_VOLUME
+--WHERE {조건절}
+--ORDER BY {컬럼 리스트}
+
+--실습예제
+SELECT
+   REGIONID, PRODUCTGROUP, YEARWEEK
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0001'
+AND YEARWEEK BETWEEN 201501 AND 201503
+ORDER BY YEARWEEK DESC;
+
+
+SELECT * 
+FROM KOPO_CUSTOMERDATA;
+
+SELECT DISTINCT GENDER 
+FROM KOPO_CUSTOMERDATA;
+
+---------------------------------------------------------------------------
+---문자를 넘버링해서 정렬하기 
+-- NOT Available -> Male -> Female
+
+SELECT A.*,
+       CASE WHEN A.GENDER = 'Not Available'
+            THEN 0
+            WHEN A.GENDER = 'Male'
+            THEN 1
+            ELSE 2 END AS SORT_ORDER 
+FROM KOPO_CUSTOMERDATA A
+ORDER BY SORT_ORDER;
+
+
+SELECT A.*,
+        --LOWER (소문자 변환) UPPER (대문자 변환)
+       CASE WHEN UPPER(A.GENDER) = 'NOT AVAILABLE'
+            THEN 0
+            WHEN LOWER(A.GENDER) = 'male'
+            THEN 1
+            ELSE 2 END AS SORT_ORDER 
+FROM KOPO_CUSTOMERDATA A
+ORDER BY SORT_ORDER;
+
+
+-- 예제 : KOPO_PRODUCT_VOLUME 테이블에서 지역, 상품, 주차 
+----------오름차순 형태로  조회되게 하세요
+
+SELECT DISTINCT PRODUCTGROUP 
+FROM KOPO_PRODUCT_VOLUME;
+
+SELECT *
+FROM KOPO_PRODUCT_VOLUME
+ORDER BY REGIONID, PRODUCTGROUP, YEARWEEK;
+
+
+---------------------------------------------------------------------------
+-- 집합 연산자 (UNION, INTERSECT, MINUS)
+-- { 테이블 #1}
+-- UNION ALL / INTERSECT / MINUS
+-- { 테이블 #2}
+---------------------------------------------------------------------------
+-- UNION (합집합) -- UNION 과 UNION ALL 의 차이는 ctrl + e 로 확인해보기
+SELECT * 
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0001'
+UNION --ALL
+SELECT * 
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0002';
+
+---------------------------------------------------------------------------
+-- INTERSECT (교집합)
+SELECT YEARWEEK
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0001'
+INTERSECT
+SELECT YEARWEEK
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0002';
+
+---------------------------------------------------------------------------
+-- MINUS (차집합)
+SELECT YEARWEEK
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0001'
+MINUS
+SELECT YEARWEEK
+FROM KOPO_PRODUCT_VOLUME
+WHERE PRODUCTGROUP = 'ST0002';
+ 
+-- NOT IN (차집합)
+SELECT *
+FROM KOPO_PRODUCT_VOLUME
+WHERE 1=1
+AND PRODUCTGROUP = 'ST0001'
+AND YEARWEEK NOT IN ( SELECT DISTINCT YEARWEEK
+                        FROM KOPO_CHANNEL_RESULT_NEW
+                    )
+ORDER BY YEARWEEK;
